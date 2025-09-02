c8e5d80c236aee4ed0bd4c03a3a030a99ea897e8
Damir Garifullin <gosugdr@gmail.com>
2025-09-03 02:56:17 +0300

    chore: Remove comments from flake.nix

--- flake.nix
+++ flake.nix
@@ -8,28 +8,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # Here we can define various kinds of "outputs": packages, tests, 
-  # and so on, but we will only define a development shell.
-
   outputs = { nixpkgs, flake-utils, ... }:
 
     # For every platform that Nix supports, we ...
     flake-utils.lib.eachDefaultSystem (system:
 
-      # ... get the package set for this particular platform ...
       let pkgs = import nixpkgs { inherit system; };
       in 
       {
 
-        # ... and define a development shell for it ...
         devShells.default =
 
-          # ... with no globally-available CC toolchain ...
           pkgs.mkShellNoCC {
             name = "rules_nixpkgs_shell";
 
-            # ... which makes available the following dependencies, 
-            # all sourced from the `pkgs` package set:
             packages = with pkgs; [ 
               bazel_7 
               bazel-buildtools 
